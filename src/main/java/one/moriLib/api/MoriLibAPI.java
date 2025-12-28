1c80cd6851d16e2b1a4e4f4ab5f149d2a87abad5
Charli <123391187+CharliVanNood@users.noreply.github.com>
2025-12-28 22:52:26 +0100

    added documentation to the api endpoint

--- src/main/java/one/moriLib/api/MoriLibAPI.java
+++ src/main/java/one/moriLib/api/MoriLibAPI.java
@@ -1,5 +1,11 @@
 package one.moriLib.api;
 
 public interface MoriLibAPI {
+    /**
+     * Registers a new block.
+     *
+     * @param name    your block's name
+     * @param texture the path to the texture
+     */
     void registerBlock(String name, String texture);
 }
